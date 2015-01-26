d5838362393218a959dcb655ead275f81e381049
unknown <konin081@D0117108.wurnet.nl>
2015-01-26 18:10:20 +0100

    s

--- R/JsontoCsvWriter.R
+++ R/JsontoCsvWriter.R
@@ -17,7 +17,9 @@ JsonToCsvWriter <- function(file) {
     latlist[i] <- raw_data$results[[i]]$geometry$location[1]
     lonlist[i] <- raw_data$results[[i]]$geometry$location[2]
     namelist[i] <- raw_data$results[[i]]$name
-    addresslist[i] <- raw_data$results[[i]]$vicinity
+    addressAdjusted <- raw_data$results[[i]]$vicinity
+    addressAdjusted <- gsub(",", "", addressAdjusted, fixed = TRUE)
+    addresslist[i] <- addressAdjusted
     typescript <- ""
     for(j in 1:length(raw_data$results[[i]]$types)) {
       typescript <- paste(typescript,raw_data$results[[i]]$types[j])
